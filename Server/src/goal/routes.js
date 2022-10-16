f73f360d7c4f28a9301392382660aaaa75ab8824
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-10-15 23:19:16 -0700

    Add error catching to /goal endpoint

--- Server/src/goal/routes.js
+++ Server/src/goal/routes.js
@@ -6,13 +6,20 @@ const { addToVisionBoard } = require("./queries");
 router.post("/", async (req, res) => {
   const { img1, img2, img3 } = req.body;
 
-  const parametersPresent = img1 && img2 && img3;
-  if (!parametersPresent) {
-    console.log("Not enough parameters in POST /goal route");
-    res.status(400).send("Not enough parameters in POST /goal route");
-  } else {
-    const result = await pool.query(addToVisionBoard, [img1, img2, img3]);
-    res.json({ message: "Successful" });
+  try {
+    const parametersPresent = img1 && img2 && img3;
+    if (!parametersPresent) {
+      console.log("Not enough parameters in POST /goal route");
+      res
+        .status(400)
+        .json({ message: "Not enough parameters in POST /goal route" });
+    } else {
+      const result = await pool.query(addToVisionBoard, [img1, img2, img3]);
+      res.json({ message: "Successful" });
+    }
+  } catch (e) {
+    console.log("Error in POST /goal route", e);
+    res.status(400).json({ message: "Error in POST /goal route" });
   }
 });
 
