49f5ffd3e97266dea145b7cc5a572286e776a223
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-10-15 22:23:28 -0700

    Add ability to aggregate and sort transactions

--- Server/src/transaction/routes.js
+++ Server/src/transaction/routes.js
@@ -4,10 +4,37 @@ const pool = require("../database/db");
 const { getAllTransactions } = require("./queries");
 
 router.get("/", async (req, res) => {
-  let query = await pool.query(getAllTransactions);
-  let result = query.rows;
-
-  res.json(result);
+  try {
+    const { rows } = await pool.query(getAllTransactions);
+    const aggregatedTransactions = aggregateData(rows);
+    const sorted = aggregatedTransactions.sort(sortDescending);
+    res.json(sorted);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send("error");
+  }
 });
+
+function sortDescending(a, b) {
+  return b.amount - a.amount;
+}
+
+function aggregateData(transactions) {
+  let singles = [];
+
+  transactions.map((transaction) => {
+    const found = singles.findIndex((element) => {
+      return element.merchant_name === transaction.merchant_name;
+    });
+    if (found === -1) {
+      console.log(transaction.merchant_name);
+      singles.push(transaction);
+    } else {
+      singles[found].amount = singles[found].amount + transaction.amount;
+    }
+  });
+
+  return singles;
+}
 
 module.exports = router;
