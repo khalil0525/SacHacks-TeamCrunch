fd37f2b50330bbd2b065cca62dae5b6d8fab23a1
khalil0525 <collinskhalil@hotmail.com>
2022-10-15 22:54:40 -0400

    added components to UserInformation

--- Client/sachacks-fe/src/pages/UserInformation.js
+++ Client/sachacks-fe/src/pages/UserInformation.js
@@ -1,3 +1,30 @@
-const UserInformation = (props) => {};
+import {
+  Grid,
+  CssBaseline,
+  Button,
+  Card,
+  CardMedia,
+  makeStyles,
+} from "@mui/material";
+// import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+}));
+
+const UserInformation = (props) => {
+  const classes = useStyles();
+  return (
+    <Grid container component="main" className={classes.root}>
+      <Card>
+        <CardMedia img="/images/placeholder.png" />
+        <CardMedia img="/images/placeholder.png" />
+        <CardMedia img="/images/placeholder.png" />
+      </Card>
+    </Grid>
+  );
+};
 
 export default UserInformation;
