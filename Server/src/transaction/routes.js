0a518c58ec185ea5789b7caa31ed1bfb8667125c
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-10-15 22:40:31 -0700

    Add query parameters to the transactions endpoint

--- Server/src/transaction/routes.js
+++ Server/src/transaction/routes.js
@@ -1,14 +1,31 @@
 const { Router } = require("express");
 const router = Router();
 const pool = require("../database/db");
-const { getAllTransactions } = require("./queries");
+const { getAllTransactions, getTransactionsInRange } = require("./queries");
 
 router.get("/", async (req, res) => {
   try {
-    const { rows } = await pool.query(getAllTransactions);
-    const aggregatedTransactions = aggregateData(rows);
-    const sorted = aggregatedTransactions.sort(sortDescending);
-    res.json(sorted);
+    const aggregate = req.query.aggregate;
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
+
+    if (startDate && endDate) {
+      const { rows } = await pool.query(getTransactionsInRange, [
+        startDate,
+        endDate,
+      ]);
+      const aggregatedTransactions = aggregateData(rows);
+      const sorted = aggregatedTransactions.sort(sortDescending);
+      res.json(sorted);
+    } else if (aggregate) {
+      const { rows } = await pool.query(getAllTransactions);
+      const aggregatedTransactions = aggregateData(rows);
+      const sorted = aggregatedTransactions.sort(sortDescending);
+      return sorted;
+    } else {
+      const { rows } = await pool.query(getAllTransactions);
+      res.json(rows);
+    }
   } catch (e) {
     console.log(e);
     res.status(400).send("error");
